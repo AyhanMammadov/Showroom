50eef6dc603d6f8b222c0eada3ca223cee178378
Y_Salon03 <Y_Salon03@pc.az>
2023-10-02 16:09:17 +0400

    Uptade window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Showroom
 {
@@ -20,9 +22,25 @@ namespace Showroom
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1); // Обновление каждую секунду
+            timer.Tick += TimerTick;
+            timer.Start();
+        }
+
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            DateTextBlock.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            DayOfWeekTextBlock.Text = DateTime.Now.ToString("dddd" , new CultureInfo("en-US"));
+            TimeTextBlock.Text = DateTime.Now.ToString("HH:mm:ss");
         }
     }
 }
